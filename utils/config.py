2deb28e809a610cefbe31d7df11fd124db9631be
Cameron Fabbri <cameron.fabbri@gmail.com>
2016-06-13 15:31:27 -0400

    just a path change

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,6 @@
 # directory of your root data, must not end with a /
-data_dir = "/home/fabbric/data_dir"
+#data_dir = "/home/fabbric/data_dir"
+data_dir = "/mnt/data3/data_dir"
 
 # specific dataset. assumes data_dir/dataset/images
 dataset  = "birds_2011"
