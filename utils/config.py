219e80f8ad0b02e0942d69940d83d3a95ae574c2
Cameron Fabbri <cameron.fabbri@gmail.com>
2016-06-14 15:40:13 -0400

    config

--- utils/config.py
+++ utils/config.py
@@ -1,11 +1,11 @@
 # directory of your root data, must not end with a /
-data_dir = "/home/fabbric/data_dir"
-#data_dir = "/mnt/data3/data_dir"
+#data_dir = "/home/fabbric/data_dir"
+data_dir = "/mnt/data3/data_dir"
 
 # specific dataset. assumes data_dir/dataset/images
 dataset  = "birds_2011"
 
-# 
+#
 train_perc = 0.85
 test_perc  = 0.1
 val_perc   = 0.05
